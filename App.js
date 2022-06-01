3593abcced71b425fecb8f6e2b488350d66ef29e
ranawp <ranasaha4224#gmail.com>
2022-06-01 15:06:06 +0600

    countries search project

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Countries from './Components/Countries';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Visiting Countries!</Text>
+      <Countries></Countries>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
